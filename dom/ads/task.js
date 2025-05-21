488dc0150fb0b5aa03f2808ccd8596a0fa34519d
Sergei Krasnov <i@sfroker.ru>
2025-05-21 09:48:24 +0300

    first commit ads

---
+++ dom/ads/task.js
@@ -0,0 +1,10 @@
+  document.querySelectorAll('.rotator').forEach(rotator => {
+    const cases = rotator.querySelectorAll('.rotator__case');
+    let index = 0;
+
+    setInterval(() => {
+      cases[index].classList.remove('rotator__case_active');
+      index = (index + 1) % cases.length;
+      cases[index].classList.add('rotator__case_active');
+    }, 1000);
+  });
